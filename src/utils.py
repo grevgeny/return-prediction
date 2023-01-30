33dc4786d142e48bc72afbdfc8cffde35dd85a68
Evgeny Grigorenko <eu.grigorenko@gmail.com>
2023-01-30 04:54:03 +0300

    Add utils.py with functions to read h5 files

---
+++ src/utils.py
@@ -0,0 +1,58 @@
+from typing import Tuple
+
+import h5py
+import pandas as pd
+
+
+def read_data_h5_file(path: str, depth: int) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Reads data from data HDF5 file and returns the order book and trades data.
+
+    Args:
+        path (str): The path to the HDF5 file.
+        depth (int): The number of levels to include in the order book data.
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame]: A tuple of two pandas DataFrames, one for the order book data and one for the trades data.
+    """
+    OB_dfs = []
+    trades_dfs = []
+
+    with h5py.File(path, "r") as f:
+        for group in ["OB", "Trades"]:
+            for name, data in f[group].items():
+                if name in ["TS", "Amount", "Price"]:
+                    data_array = data[()]
+                    data_df = pd.DataFrame(data_array, columns=[name])
+                else:
+                    data_array = data[:, :depth]
+                    data_df = pd.DataFrame(
+                        data_array,
+                        columns=[f"{name.lower()}_{i}" for i in range(1, depth+1)],
+                        dtype="float32"
+                    )
+                
+                if group == "OB":
+                    OB_dfs.append(data_df)
+                else:
+                    trades_dfs.append(data_df)
+
+    OB_df = pd.concat(OB_dfs, axis=1)
+    trades_df = pd.concat(trades_dfs, axis=1)
+
+    return OB_df, trades_df
+
+
+def read_result_h5_file(path: str) -> pd.DataFrame:
+    """Reads data from HDF5 file and returns mid-price returns data.
+    
+    Args:
+        path (str): The path to the HDF5 file.
+    
+    Returns:
+        pd.DataFrame: A pandas DataFrame containing the result data from the file.
+    """
+    with h5py.File(path, "r") as f:
+        data_array = f["Return/Res"][()]
+        returns_df = pd.DataFrame(data_array, columns=["returns"], dtype="float32")
+    
+    return returns_df

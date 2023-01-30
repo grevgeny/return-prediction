5af44586a162107820e59da50ec4f8826eed041a
Evgeny Grigorenko <eu.grigorenko@gmail.com>
2023-01-30 19:03:24 +0300

    Finalize main script

--- src/run_forecasting.py
+++ src/run_forecasting.py
@@ -2,45 +2,50 @@ import argparse
 
 from loguru import logger
 
-from utils import save_results
 from data_preprocessing import load_data, process_data
-from modelling import fit, save_model, load_model, forecast
+from modelling import fit, forecast, load_model, save_model
+from utils import save_results
 
 
 def main():
     parser = argparse.ArgumentParser(description="Forecast mid return for cryptoasset")
-    parser.add_argument("--mode", type=str, choices=["fit", "forecast"], help="Mode of operation")
-    parser.add_argument("--train_data_path", type=str, help="Path to training data directory with data.h5 and result.h5")
-    parser.add_argument("--forecast_data_path", type=str, help="Path to forecasting data file data.h5")
-    parser.add_argument("--output_path", type=str, nargs="?", default="./", help="Directory to save forecast.h5 file (optional)")
+    parser.add_argument(
+        "--mode", 
+        type=str, 
+        choices=["fit", "forecast"], 
+        help="Mode of operation"
+    )
+    parser.add_argument(
+        "--data_path", 
+        type=str, 
+        help="(fit) Path to training data directory with data.h5 and result.h5 or (forecast) path to data.h5"
+    )
     args = parser.parse_args()
 
     mode = args.mode
+    data_path = args.data_path
 
     # Load data
-    logger.info("Preparing data...")
-    OB_df, trades_df = load_data(args.train_path, mode)
+    logger.info("Loading data...")
+    OB_df, trades_df = load_data(data_path, mode)
+    logger.info("Data was loaded successfully\n")
 
     # Process data
+    logger.info("Processing data...")
     df, TS = process_data(OB_df, trades_df)
     del trades_df
+    logger.info("Data processed successfully\n")
 
     if mode == "fit":
-    
-        # Split data
-        X = df.drop(columns=["returns"])
-        y = df["returns"]
-        del df
-
         # Fit model
         logger.info("Training model...")
-        model = fit(X, y)
-        logger.info("Model trained successfully\n")
+        model = fit(df)
+        logger.info("Model training completed successfully\n")
 
         # Save model
         logger.info("Saving model...")
         save_model(model)
-        logger.info("Model saved to ./model\n")
+        logger.info("Model saved to ./models/catboost_model\n")
 
     elif args.mode == "forecast":
 
@@ -51,13 +56,14 @@ def main():
         
         # Forecast with fitted model
         logger.info("Forecasting...")
-        y_pred = forecast(df, model)
-        logger.info(f"Done!\n")
+        y_preds = forecast(df, model)
+        logger.info("Forecasting completed successfully\n")
 
-        # Save forecats
+        # Save forecast
         logger.info("Saving results...")
-        save_results(y_pred, TS)
+        save_results(y_preds, TS)
         logger.info(f"Results saved in ./forecast.h5\n")
+
 
 if __name__ == "__main__":
     main()
